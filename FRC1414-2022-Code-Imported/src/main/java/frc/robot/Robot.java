728cd4acfe4441e43786d673db97f4bc1967602e
SuperAce100 <46036121+SuperAce100@users.noreply.github.com>
2022-03-16 16:54:42 -0400

    Add realsense

--- FRC1414-2022-Code-Imported/src/main/java/frc/robot/Robot.java
+++ FRC1414-2022-Code-Imported/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import frc.util.Realsense;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -12,6 +14,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  private Realsense realsense;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -22,6 +25,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    realsense = Realsense.getInstance();
   }
 
   /**
@@ -51,7 +55,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
+    realsense.zero();
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
